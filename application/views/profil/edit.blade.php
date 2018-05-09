5448497f46ca1f04b3cb3f1ed293cae9a530fa8e
mnindrazaka <mnindrazaka@gmail.com>
2018-05-09 09:43:04 +0700

    map picker untuk merubah lokasi

--- application/views/profil/edit.blade.php
+++ application/views/profil/edit.blade.php
@@ -46,16 +46,14 @@
                     </div>
 
                     <div class="form-group">
-                        <label for="latitude">Latitude</label>
-                        <input id="latitude" type="text" class="form-control" name="latitude" value="{{ old('latitude', $user->latitude) }}" placeholder="Masukkan Latitude">
+                        <input id="latitude" type="hidden" class="form-control" name="latitude" value="{{ old('latitude', $user->latitude) }}" placeholder="Masukkan Latitude">
+                        <input id="longitude" type="hidden" class="form-control" name="longitude" value="{{ old('longitude', $user->longitude) }}" placeholder="Masukkan Longitude">
+
+                        <div id="googleMap" style="width:100%;height:300px;"></div>
+                        <small class="text-muted">klik pada peta untuk merubah lokasi</small>
                         @if($errors->has('latitude'))
                             <small class="text-danger">{{ $errors->first('latitude') }}</small>
                         @endif
-                    </div>
-
-                    <div class="form-group">
-                        <label for="longitude">Longitude</label>
-                        <input id="longitude" type="text" class="form-control" name="longitude" value="{{ old('longitude', $user->longitude) }}" placeholder="Masukkan Longitude">
                         @if($errors->has('longitude'))
                             <small class="text-danger">{{ $errors->first('longitude') }}</small>
                         @endif
@@ -64,4 +62,44 @@
             </div>
         </form>
     </div>
+@endsection
+
+@section('script')
+    <script>
+        function myMap() {
+            // Make Maps
+            var myCenter = new google.maps.LatLng({{ $user->latitude }}, {{ $user->longitude }});
+            var mapProp = {
+                center: myCenter,
+                zoom: 15
+            };
+            var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+            var marker = new google.maps.Marker({
+                position: myCenter,
+                map: map
+            });
+
+            // Add Click Event
+            google.maps.event.addListener(map, 'click', function (event) {
+                placeMarker(map, event.latLng);
+                document.getElementById("latitude").value = event.latLng.lat();
+                document.getElementById("longitude").value = event.latLng.lng();
+            });
+
+            // Place Marker
+            function placeMarker(map, location) {
+                // drop previous marker
+                if(marker) {
+                    marker.setMap(null)
+                }
+
+                marker = new google.maps.Marker({
+                    position: location,
+                    map: map
+                })
+            }
+        }
+    </script>
+
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB-0iUmT6UROU_Hx4RJtY32o2vSxS6PQS4&callback=myMap"></script>
 @endsection
