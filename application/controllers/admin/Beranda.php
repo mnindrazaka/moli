3f91c1d05d79abdf48fbdfcf44a784b8d7bc22dc
mohberliannusantara <liannusantara1158@gmail.com>
2018-05-11 21:24:15 +0000

    installing template admin

---
+++ application/controllers/admin/Beranda.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Beranda extends MY_Controller {
+
+  public function index()
+  {
+    $this->view('admin/template');
+  }
+}
---
+++ application/controllers/admin/Login.php
@@ -0,0 +1,6 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends MY_Controller {
+
+}
